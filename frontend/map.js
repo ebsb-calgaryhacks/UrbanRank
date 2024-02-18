2d44129ff85e64c0a4cbdd8673d67e3e9fd9e517
Jacob Feng <peanutchowder@gmail.com>
2024-02-18 02:09:22 -0700

    Added polygon drawing

--- frontend/map.js
+++ frontend/map.js
@@ -1,21 +1,30 @@
 
 var map;
 let lat, lng;
+let boundaries;
 
 async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-    if (navigator.geolocation) {
-        const geolocation = await new Promise((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition(resolve, reject);
-        });
+  const { Map } = await google.maps.importLibrary("maps");
+  if (navigator.geolocation) {
+    const geolocation = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 
-        [lat, lng] = [geolocation.coords.latitude, geolocation.coords.longitude]
+    [lat, lng] = [geolocation.coords.latitude, geolocation.coords.longitude]
 
-        map = await new Map(document.getElementById("map"), {
-            center: { lat: lat, lng: lng },
-            zoom: 5,
-        })        
+    map = await new Map(document.getElementById("map"), {
+      center: { lat: lat, lng: lng },
+      zoom: 12,
+    })
+
+    let res = await fetch("http://127.0.0.1:5000/boundaries")
+    let temp = await res.json()
+    boundaries = temp.boundaries
+
+    for (const [key, coords] of Object.entries(boundaries)) {
+      drawPolygon(coords, "#FF0000")
     }
+  }
 }
 
 /**
@@ -24,21 +33,33 @@ async function initMap() {
  * @param {String} colour A hex string
  */
 const drawPolygon = (coords, colour) => {
-    let res = coords.map((point) => {
-        return { lat: point[0], lng: point[1] }
-    })
+  let res = coords.map((point) => {
+    const lat = Number(point[0])
+    const lng = Number(point[1])
+
+    return {
+      lat: isNaN(lat) ? 0: lat,
+      lng: isNaN(lng) ? 0: lng
+    }
+  })
+
+  console.log(res);
+  
 
 
-    const polygon = new google.maps.Polygon({
-        path: res,
-        strokeColor: colour,
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: colour,
-        fillOpacity: 0.35,
-    })
+  const polygon = new google.maps.Polygon({
+    path: res,
+    strokeColor: colour,
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: colour,
+    fillOpacity: 0.35,
+  })
 
-    polygon.setMap(map);
+
+  polygon.setMap(map);
+
+
 }
 
 initMap();
