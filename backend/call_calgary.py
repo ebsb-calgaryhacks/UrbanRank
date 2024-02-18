b591ffda1701d9b503e827f44eb1c01f8f1ee19a
Brant-Harker <brant.harker12@gmail.com>
2024-02-17 20:18:42 -0700

    get community points and schools

--- backend/call_calgary.py
+++ backend/call_calgary.py
@@ -8,13 +8,11 @@ load_dotenv("./backend/.env")
 
 client = Socrata("data.calgary.ca", os.environ.get("MyAppToken"))
 
-def call_calgary_api(**kwargs):
+def call_calgary_api(database, **kwargs):
     """
-    Queries the calgary data api. Pass in sql arguments, returns a json.
+    Queries the calgary data api. Database is the id of the database in the calgary data portal. kwargs is the sql arguments, returns a json.
     """
-    endpoint = "xeek-u7v8"
-
-    return client.get(endpoint, **kwargs, limit = 3000)
+    return client.get(database, **kwargs, limit = 3000)
 
 def get_data(indicators):
     """
@@ -28,12 +26,38 @@ def get_data(indicators):
     
     where = where[:-4]
 
-    data = pd.DataFrame.from_records(call_calgary_api(where = where))
+    data = pd.DataFrame.from_records(call_calgary_api("xeek-u7v8", where = where))
 
     return data[["communities", "indicator", "value"]]
 
+def get_community_points():
+    """
+    Returns community_name, longitude, and latitude of points
+    """
+
+    data = pd.DataFrame.from_records(call_calgary_api("j9ps-fyst"))
+
+    return data[["name", "longitude", "latitude"]]
+
+def get_schools():
+    """
+    Returns school name, boolean whether it's elementary, juniour high, or senior high, and the longitude and latitude
+    """
+
+    data = pd.DataFrame.from_records(call_calgary_api("fd9t-tdn2"))
+
+    data["longitude"] = data["point"].apply(lambda x: x['coordinates'][0])
+    data["latitude"] = data["point"].apply(lambda x: x['coordinates'][1])
+    
+    return data[["name", "elem", "junior_h", "senior_h", "longitude", "latitude"]]
 
 if __name__ == '__main__':
-    #results = call_calgary_api(where = "INDICATOR='LOW INCOME' OR INDICATOR='BIKE SCORE'")
-    results = get_data(["BIKE SCORE"])
-    print(results)
+    # results = call_calgary_api(where = "INDICATOR='LOW INCOME' OR INDICATOR='BIKE SCORE'")
+    # results = get_data(["BIKE SCORE"])
+    # print(results)
+
+    points = get_community_points()
+    print(points)
+
+    schools = get_schools()
+    print(schools)
