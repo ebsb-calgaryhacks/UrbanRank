0597493c49bf6722666a60b98d33b7c6d35168de
Jacob Feng <peanutchowder@gmail.com>
2024-02-18 01:06:32 -0700

    Fixed map contents

--- frontend/map.js
+++ frontend/map.js
@@ -1,12 +1,46 @@
-let map;
+
+var map;
+let lat, lng;
 
 async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+    const { Map } = await google.maps.importLibrary("maps");
+    if (navigator.geolocation) {
+        const geolocation = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+        [lat, lng] = [geolocation.coords.latitude, geolocation.coords.longitude]
+
+        map = await new Map(document.getElementById("map"), {
+            center: { lat: lat, lng: lng },
+            zoom: 5,
+        })        
+    }
+    let res = getBoundaries()
+}
+
+/**
+ * 
+ * @param {Array<Array<number>>} coords An array of coordinate arrays (2 element inner arrays)
+ * @param {String} colour A hex string
+ */
+const drawPolygon = (coords, colour) => {
+    let res = coords.map((point) => {
+        return { lat: point[0], lng: point[1] }
+    })
+
+
+    const polygon = new google.maps.Polygon({
+        path: res,
+        strokeColor: colour,
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: colour,
+        fillOpacity: 0.35,
+    })
+
+    polygon.setMap(map);
 }
 
 initMap();
+
